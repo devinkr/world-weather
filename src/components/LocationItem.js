d733b34edb6f8305db6a330ea1cbcd300cf33853
Devin Raleigh <devinkr@gmail.com>
2022-06-17 15:47:38 -0600

    Add API to LocationItems

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -1,11 +1,30 @@
-function LocationItem(props) {
+import { useState, useEffect } from 'react';
+import getWeatherData from '../getWeatherData';
+
+function LocationItem({ time, location, index }) {
+	const [locationData, setLocationData] = useState(null);
+
+	useEffect(() => {
+		getWeatherData(location, setLocationData);
+	}, []);
+
+	if (!locationData) {
+		return 'nope';
+	}
+
 	return (
 		<div className='locations-item'>
 			<div className='locations-item-citytime'>
-				<div>Denver</div>
-				<div className='locations-item-time'>8:52 PM</div>
+				<div>{locationData.location.name}</div>
+				<div className='locations-item-time'>
+					{time.toLocaleTimeString([], {
+						hour: 'numeric',
+						minute: 'numeric',
+						timeZone: locationData.location.tz_id,
+					})}
+				</div>
 			</div>
-			<div className='locations-item-temp'>83° 53°</div>
+			<div className='locations-item-temp'>{locationData.current.temp_f}°</div>
 		</div>
 	);
 }
