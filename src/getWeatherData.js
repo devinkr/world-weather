85c53cdb60bea06a65d0b20824e09f496ee6fb31
Devin Raleigh <devinkr@gmail.com>
2022-06-17 21:42:03 -0600

    No CORS?

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,6 +1,6 @@
 export default function getWeatherData(location, setLocationData) {
 	const url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHERAPI_KEY}&q=${location}&days=3&aqi=no&alerts=no`;
-	fetch(url, { crossDomain: true, mode: 'cors' })
+	fetch(url, { crossDomain: true, mode: 'no-cors' })
 		.then((res) => res.json())
 		.then((data) => {
 			setLocationData(data);
