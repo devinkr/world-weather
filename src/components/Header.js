a982e97e4328ba00d1883a3c116239dc3c81a0c9
Devin Raleigh <devinkr@gmail.com>
2022-06-23 12:41:50 -0600

    Save units choice to localStorage

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,37 @@ function Header({ units, setUnits }) {
 			<h1>World Weather</h1>
 			<div className='units'>
 				<button
-					onClick={() => setUnits('f')}
+					onClick={() => {
+						setUnits('f');
+						const worldWeatherLocal = JSON.parse(
+							localStorage.getItem('worldWeatherLocal')
+						);
+						localStorage.setItem(
+							'worldWeatherLocal',
+							JSON.stringify({
+								...worldWeatherLocal,
+								units: 'f',
+							})
+						);
+					}}
 					disabled={units === 'f'}
 					aria-label='Set units to Fahrenheit'>
 					F
 				</button>
 				<button
-					onClick={() => setUnits('c')}
+					onClick={() => {
+						setUnits('c');
+						const worldWeatherLocal = JSON.parse(
+							localStorage.getItem('worldWeatherLocal')
+						);
+						localStorage.setItem(
+							'worldWeatherLocal',
+							JSON.stringify({
+								...worldWeatherLocal,
+								units: 'c',
+							})
+						);
+					}}
 					disabled={units === 'c'}
 					aria-label='Set units to Celsius'>
 					C
